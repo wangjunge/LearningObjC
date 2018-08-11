7bc06db8f658f08a7e191b790bdc94128b9a8a41
Junge Wang <wangjunge@gmail.com>
2018-08-11 11:22:49 +0800

    Initial Commit

---
+++ LearningObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearningObjC
+//
+//  Created by Junge Wang on 11/8/18.
+//  Copyright © 2018 Junge Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearningObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningObjC
+//
+//  Created by Junge Wang on 11/8/18.
+//  Copyright © 2018 Junge Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
